1249498b483112798c37ec8ea18d0e30c9805af9
amybartolotta <amy.bartolotta@ncf.edu>
2017-07-27 22:53:21 -0700

    Create theme from template

--- index.php
+++ index.php
@@ -13,30 +13,44 @@
 <header>
         <a href="index.php"><h1>AUDREY GLADSON</h1></a>
         <a href="index.php"><h3>Actor - Chicago, IL</h3></a>
-    <nav id="cssmenu">
-        <div class="menubutton"></div>
-        <ul class="menulist">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="resume.php">Resume</a></li>
-            <li><a href="media.php">Media</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="news.php">News</a></li>
-        </ul>
-    </nav>
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => 'div','container_id' => 'navigation', 'items_wrap' => '<ul id="navigation_items" class="%2$s">%3$s</ul>', ) ); ?>
 
+<div id="sidebar">
+	<?php if(is_page()) : ?>
+	<h2><?php echo get_the_title($post->post_parent);?></h2>
+	<ul class="sub-navigation-items">
+	<?php 
+	if ($post->post_parent) {
+
+	wp_list_pages(array( 'child_of' => $post->post_parent, 'title_li' => '',)); 
+} else {
+	wp_list_pages(array('child_of' => $post->ID, 'title_li' => '',));
+}
+?>
+</ul>
+<?php endif; ?>
+
+<?php if(!(is_page())) : ?>
+<h2>Blog</h2>
+<ul class="sub-navigation-items">
+<?php wp_list_categories(array('title_li' => '',)); ?>	
+<?php endif; ?>
+	
+</div>	
+    
     <div id="content"><?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
-    <h2><a href="<?php the_permalink(); // link to the page or posting ?>">
-    <?php the_title(); // get the page or posting title ?></a></h2>
-    <?php the_content(''); // get page or posting written content ?>
+      <?php the_content(''); // get page or posting written content ?>
     <?php endwhile; endif; // end the loop ?>
     </div>
+
+	
+
     </header> 
         <!-- START LEFT COLUMN -->
             <div id='hs1' class="wrapper">
             </div>
-
 <?php wp_footer(); ?>
+
 
 </body>
 </html>
