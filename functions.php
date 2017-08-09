ca581341f14876849a70005ea73fcc13d1017939
amybartolotta <amy.bartolotta@ncf.edu>
2017-08-09 13:40:28 -0700

    Creating a Custom Posts Feed with Featured Images

--- functions.php
+++ functions.php
@@ -16,18 +16,22 @@ This theme, like WordPress, is licensed under the GPL.
 Use it to make something cool, have fun, and share what you've learned with others.
 */
 
-register_sidebar(array(
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget'  => '</div>',
-	'before_title'  => '<h2>',
-	'after_title'   => '</h2>'
-));
 
-
-// Register My Menus
-register_nav_menus(array(
-'main-menu' => __( 'Main' ),
-));
+// Register Sidebars
+register_sidebars(5, array('before_widget' => '<div id="%1$s" class="widget %2$s">', 'after_widget' => '</div>'));
+//
+register_sidebar(array('name' =>  __('Fred', 'fred'), 'before_widget' => '<div id="%1$s" class="widget %2$s">', 'after_widget' => '</div>' ));
+// Register Navigation Menus 
+register_nav_menus(array('main-menu' => __('Main Menu')));
+//
+// Add Supprt for Post Thumbnails & Featured Images 
+add_theme_support('post-thumbnails');
+//
+// Create Custom Image Sizes
+add_image_size('icon', 140, 140, true); // 140 pixels wide by 140 pixels tall, hard crop mode
+//
+// Excerpt for Pages
+add_post_type_support('page', 'excerpt');
 //
 
 
