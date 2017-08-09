aa5df6b6b2592b99a74d94a42960fb90350b766d
amybartolotta <amy.bartolotta@ncf.edu>
2017-08-09 13:39:40 -0700

    Creating a Custom Posts Feed with Featured Images

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php get_header(); ?>
 
     <div id="content"><?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
+	<article class="post-excerpt">
     <h2><a href="<?php the_permalink(); // link to the page or posting ?>">
     <?php the_title(); // get the page or posting title ?></a></h2>
-    <?php the_content(''); // get page or posting written content ?>
+	<small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>
+</small>
+	<a href="<?php the_permalink(); ?>">
+	<?php the_post_thumbnail( 'thumbnail' ); ?>
+	<?php the_excerpt(); // get the posting's excerpt ?>
+	</article>
     <?php endwhile; endif; // end the loop ?>
     <small>index.php</small>
     </div>
