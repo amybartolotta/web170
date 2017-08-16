ca88a11d243e1b933a098f8d4f02e259ef15d0f7
amybartolotta <amy.bartolotta@ncf.edu>
2017-08-15 17:36:00 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -34,5 +34,41 @@ add_image_size('icon', 140, 140, true); // 140 pixels wide by 140 pixels tall, h
 add_post_type_support('page', 'excerpt');
 //
 
+// Get My Title Tag
+function get_my_title_tag() {
+	
+	global $post; // make sure you make this a global variable
+	
+	if ( is_front_page() ) {  // for the site’s Front Page
+	
+		bloginfo('description'); // retrieve the site tagline
+	
+	} 
+	
+	elseif ( is_page() || is_single() ) { // for your site’s Pages or Postings
+	
+		the_title(); // retrieve the page or posting title 
+	
+	} 
+	
+	else { // for everything else
+		
+		bloginfo('description'); // retrieve the site tagline
+		
+	}
+	
+	if ( $post->post_parent ) { // for your site’s Parent Pages
+	
+		echo ' | '; // separator with spaces
+		echo get_the_title($post->post_parent);  // retrieve the parent page title
+		
+	}
+	echo ' | '; // separator with spaces
+	bloginfo('name'); // retrieve the site name
+	echo ' | '; // separator with spaces
+	echo 'Seattle, WA.'; // write in the location
+	
+}
+//
 
 ?>
