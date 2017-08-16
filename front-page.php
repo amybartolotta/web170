41a405c7760065164d6e0c9303093e53a54b40cd
amybartolotta <amy.bartolotta@ncf.edu>
2017-08-15 17:34:55 -0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -5,6 +5,12 @@
     <?php the_title(); // get the page or posting title ?></a></h2>
     <?php the_content(''); // get page or posting written content ?>
     <?php endwhile; endif; // end the loop ?>
+	<h2>Latest Posts</h2>
+	<?php rewind_posts(); // stop loop one ?>
+	<?php query_posts('showposts=4'); // give directions to loop two ?>
+	<?php while (have_posts()) : the_post(); // start loop two ?>
+	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+	<?php endwhile; // end loop two ?>
     <small>front-page.php</small>
     </div>
     </header> 
